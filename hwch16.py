109277eef47328b7e4a3aa8958b4a72d53e4b4a0
Nibalizer <krum.spencer@gmail.com>
2010-10-31 18:30:13 -0700

    added prob31

--- hwch16.py
+++ hwch16.py
@@ -6,7 +6,7 @@ from pylab import *
 
 import numpy as np
 
-#print "Hello and welcome to Spencer Krum\'s Physical Chemistry homework"
+print "Hello and welcome to Spencer Krum\'s Physical Chemistry homework"
 
 
 def twenty(x):
@@ -23,9 +23,57 @@ def RK(V, R, T, A, B):
     return p
 
 
+def sixteen_fortyseven
+
+
+
 #for i in range(10):
 #    print twenty(i)
 
+def sixteen_thirtyone(name):
+    #const
+    R_LBarMolK = 0.083145
+    R_LatmmolK = 0.082058
+
+    #ethane
+    ethane_p_bar = [.5,2, 10, 20, 40, 60, 80, 100, 120, 160, 200, 240, 300, 350, 400, 450, 500, 600]
+    ethane_T = 500
+    ethane_Vbar_literpermol = [83.076, 20.723, 4.105, 2.028, 0.9907, 0.6461, 0.4750, 0.3734, 0.3068, 0.2265, 0.1819, 0.1548, 0.1303, 0.1175, 0.1085, 0.1085, 0.1019, 0.09676, 0.08937, 0.08421]
+    ethane_Vc = 0.1480
+    #calc Z
+    ethane_z = []
+    ethane_Vr = []
+    for i in range(len(ethane_p_bar)):
+        ethane_z.append(((ethane_p_bar[i] * ethane_Vbar_literpermol[i] )/(R_LBarMolK * ethane_T ))) 
+        ethane_Vr.append(ethane_Vbar_literpermol[i] / ethane_Vc ) 
+
+    #argon
+    argon_p_atm = [.5,2, 10, 20, 40, 60, 80, 100, 120, 160, 200, 240, 300, 350, 400, 450, 500, 600]
+    argon_T = 247
+    argon_Vbar_literpermol = [40.506, 10.106, 1.999, 0.9857, 0.4795, 0.3114, 0.2279, 0.1785, 0.1462, 0.1076, 0.08630, 0.07348, 0.06208, 0.05626, 0.05219, 0.04919, 0.05687, 0.04348, 0.04108]
+    argon_Vc = 0.07530
+
+    #calc Z
+    argon_z = []
+    argon_Vr = []
+    for i in range(len(argon_p_atm)):
+        argon_z.append(((argon_p_atm[i] * argon_Vbar_literpermol[i] )/(R_LatmmolK * argon_T ))) 
+        argon_Vr.append(argon_Vbar_literpermol[i] / argon_Vc ) 
+    
+    
+    ion()
+    plot(argon_Vr, argon_z , 'g-', label="Argon")
+    plot(ethane_Vr , ethane_z, 'r-', label="Ethane")
+    ylabel('Compressibility')
+    xlabel('Reduced Molar Volume')
+    title("Problem 16.31")
+    legend(numpoints=1)
+    grid(True)
+    draw()
+    hold(True)
+    savefig(name)
+    ioff()
+    raw_input("Press return to quit")
 
 def sixteen_twenty(name):
     a = 9.3919
@@ -34,8 +82,9 @@ def sixteen_twenty(name):
     B = 0.062723
     T = 400
     R = 0.083145
-    
+
     rho = np.arange(0, 12.3, .01)
+
     P_book = []
     P_rk = []
     P_vdw = []
@@ -48,19 +97,21 @@ def sixteen_twenty(name):
         P_vdw.append(VdW((rhos**-1), R, T, a, b))
 
     ion()
-    plot(rho, P_book , 'g.')
-    plot(rho , P_rk, 'r.')
-    plot(rho1, P_vdw , 'b.')
+    plot(rho, P_book , 'g.', label="Experimental")
+    plot(rho , P_rk, 'r.', label="Redlich-Kwong")
+    plot(rho1, P_vdw , 'b.', label="van der Waals" )
     ylabel('Pressure')
     xlabel('Density')
+    legend(numpoints=1)
+    title("Problem 16.20")
     grid(True)
     draw()
-    hold(False)
+    hold(True)
     savefig(name)
+    ioff()
     raw_input("Press return to quit")
 
 
-namezz = "lies.png"
-sixteen_twenty(namezz)
-
+sixteen_thirtyone('Ch16_31.png')
+sixteen_twenty('Ch16_20.png')
 
